51abc83f3f7bfe3a6a70d2c23ae75f96c27ef0eb
Mohamed Yasser <mohamedyasser96@aucegypt.edu>
2020-03-02 11:47:00 +0200

    Shifted google signup to the user service

--- lib/landing.dart
+++ lib/landing.dart
@@ -2,61 +2,10 @@ import 'package:Steps4Cause/services/user.dart';
 import 'package:flutter/material.dart';
 import 'package:Steps4Cause/login.dart';
 import 'package:Steps4Cause/register.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:provider/provider.dart';
 
 class MyLandingPage extends StatelessWidget {
   final style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-  final GoogleSignIn googleSignIn = GoogleSignIn();
-  Widget _facebookButton() {
-    return Container(
-      height: 50,
-      margin: EdgeInsets.symmetric(vertical: 20),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.all(Radius.circular(10)),
-      ),
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            flex: 1,
-            child: Container(
-              decoration: BoxDecoration(
-                color: Color(0xff1959a9),
-                borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(5),
-                    topLeft: Radius.circular(5)),
-              ),
-              alignment: Alignment.center,
-              child: Text('f',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 25,
-                      fontWeight: FontWeight.w400)),
-            ),
-          ),
-          Expanded(
-            flex: 5,
-            child: Container(
-              decoration: BoxDecoration(
-                color: Color(0xff2872ba),
-                borderRadius: BorderRadius.only(
-                    bottomRight: Radius.circular(5),
-                    topRight: Radius.circular(5)),
-              ),
-              alignment: Alignment.center,
-              child: Text('Log in with Facebook',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18,
-                      fontWeight: FontWeight.w400)),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
 
   Widget _facebookTest() {
     return Container(
@@ -84,32 +33,6 @@ class MyLandingPage extends StatelessWidget {
         ),
       ),
     );
-  }
-
-  Future<String> signInWithGoogle(BuildContext context) async {
-    final userService = Provider.of<UserService>(context, listen: false);
-    final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
-    final GoogleSignInAuthentication googleSignInAuthentication = await googleSignInAccount.authentication;
-
-    final AuthCredential credential = GoogleAuthProvider.getCredential(
-      accessToken: googleSignInAuthentication.accessToken,
-      idToken: googleSignInAuthentication.idToken,
-    );
- 
-    final AuthResult authResult = await _auth.signInWithCredential(credential);
-    final FirebaseUser user = authResult.user;
-    assert(!user.isAnonymous);
-    assert(await user.getIdToken() != null);
-    final FirebaseUser currentUser = await _auth.currentUser();
-    assert(user.uid == currentUser.uid);
-    await userService.addSocialMediaAccount(currentUser);
-    return 'signInWithGoogle succeeded: $user';
-  }
-
-  void signOutGoogle() async{
-    await googleSignIn.signOut();
-
-    print("User Sign Out");
   }
 
   @override
@@ -244,7 +167,9 @@ class MyLandingPage extends StatelessWidget {
                       ),
                       InkWell(
                         onTap: () {
-                          signInWithGoogle(context);
+                          final userService =
+                              Provider.of<UserService>(context, listen: false);
+                          userService.signInWithGoogle(context);
                         },
                         child: _google(),
                       )
